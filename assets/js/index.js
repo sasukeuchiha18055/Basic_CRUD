c7c26efe20ea4f25d1d823bb558aa70d6e972077
sasukeuchiha18055 <77959371+sasukeuchiha18055@users.noreply.github.com>
2023-11-06 16:22:23 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `https://${hostname}:3000/api/users/${data.id}`,
+        "url" : `http://${hostname}:3000/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -34,7 +34,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `https://${hostname}:3000/api/users/${id}`,
+            "url" : `http://${hostname}:3000/api/users/${id}`,
             "method" : "DELETE"
         }
 
